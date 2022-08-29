2ec702b9acea1cb550990e0206291cc1939ec4af
Cassiano S <cassiano@scheidemantel.com.br>
2022-08-29 20:31:13 -0300

    fix: aula eventos

--- react/curso-programadorcs-parte1-app/src/Banner/Banner.js
+++ react/curso-programadorcs-parte1-app/src/Banner/Banner.js
@@ -1,12 +1,30 @@
 export const Banner = (dadosUsuario) => {
-  console.log("====================================");
-  console.log(dadosUsuario);
-  console.log("====================================");
+  // console.log("====================================");
+  // console.log(dadosUsuario);
+  // console.log("====================================");
   const { nome, idade } = dadosUsuario;
+
+  const handleClicar = () => {
+    console.log(nome);
+  };
+
+  // const handleDigitar = (e) => {
+  //   console.log(e.target.value);
+  // };
+  function handleDigitar(evento) {
+    console.log(evento.target.value);
+  }
+
   return (
     <div>
-      <h1 onClick={() => console.log(nome)}>Nom3e: {nome}</h1>
+      <h1 onClick={handleClicar}>Nom3e: {nome}</h1>
       <h3>Idade: {idade}</h3>
+      <input
+        type="text"
+        onKeyUp={(eventoEnviadoViaParam) =>
+          handleDigitar(eventoEnviadoViaParam)
+        }
+      />
     </div>
   );
 };
