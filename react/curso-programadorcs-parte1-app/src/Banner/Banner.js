01c0d42d8f0511e1b5a7bc0e2e2713243c943d0f
Cassiano S <cassiano@scheidemantel.com.br>
2022-08-29 20:29:56 -0300

    fix: aula parametros

--- react/curso-programadorcs-parte1-app/src/Banner/Banner.js
+++ react/curso-programadorcs-parte1-app/src/Banner/Banner.js
@@ -1,5 +1,14 @@
-export const Banner = () => {
-  return <h1>Cassiano</h1>;
+export const Banner = (dadosUsuario) => {
+  console.log("====================================");
+  console.log(dadosUsuario);
+  console.log("====================================");
+  const { nome, idade } = dadosUsuario;
+  return (
+    <div>
+      <h1 onClick={() => console.log(nome)}>Nom3e: {nome}</h1>
+      <h3>Idade: {idade}</h3>
+    </div>
+  );
 };
 export const Banner2 = () => {
   return <h1>Cassiano2</h1>;
--- react/curso-programadorcs-parte1-app/src/index.js
+++ react/curso-programadorcs-parte1-app/src/index.js
@@ -4,10 +4,16 @@ import ReactDOM from "react-dom/client";
 import Banner, { Banner2 } from "./Banner";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const dadosUsuario = {
+  nome: "Cassiano",
+  idade: 36,
+};
+
 root.render(
   <div>
-    <Banner />
-    <Banner2 />
+    <Banner {...dadosUsuario} />
+    {/* <Banner2 /> */}
     {/* <h1>Programador CS</h1>
     <h2>Curso de React</h2> */}
   </div>
